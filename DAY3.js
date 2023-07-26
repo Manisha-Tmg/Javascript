42a968304345a115356fd70b9f5d7a4cb90534a2
manishatamang752@gmail.com <manishatamang752@gmail.com>
2023-07-26 23:16:49 +0545

    my first commit

--- DAY3.js
+++ DAY3.js
@@ -21,3 +21,31 @@ console.log(!false)
 console.log(!true)
 /*  Tthis is multiline comments*/
 ///this is used for single line comment
+
+
+//conditional statement
+/*if 
+if...else
+if...else...if*/
+let s= prompt("Hey !whats your age")
+s=Number.parseInt(s)//converting the string to a number
+console.log(typeof s)
+if(s>0){//block of code if condition true
+    alert("This is an invalid age")
+ }
+ else{//block of code if condition false
+    alert("This is an invalid age")
+ }
+
+
+ if(s<0){
+    alert("This is an invalid age")
+ }
+ else if(s<9){
+    alert("you r just a kid")}
+else if(s<18 && a>=9){
+    alert("you can thik of driving")
+}
+  else{
+    alert("This is an invalid age")
+ }
