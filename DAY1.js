cba3f10859699924b7bd5bb3582fe71549cb0490
manishatamang752@gmail.com <manishatamang752@gmail.com>
2023-07-26 22:50:44 +0545

    my first commit

--- DAY1.js
+++ DAY1.js
@@ -1,3 +1,11 @@
+//let is a keyword that lets us declare a variable. 
+let remember ="june.2";
+var age;
+age= 39;
+var friends =["misha","isha","nisha"];
+console.log(friends.length);
+console.log(friends[1])
+
 // "misha" + "!"
 // typeof "value"
 // + is an operator with several different uses:
--- DAY3.js
+++ DAY3.js
@@ -1,97 +1,23 @@
+//Assignment operators
 
-"use strict"
-//use of use strict
-//error show garxa
-//helps avoid accidental error
-//prevents us from doing certain things.. 
-const a=10;
-var b=a+10;
-console.log(a);
-function myfunction(){
-    let a=10;
-    if(a>5){
-       let a=20;//locally variable declare garna
-       console.log(a);
+let assignment=5;
+assignment+=5// same as assignment = assignment+5
+console.log(assignment)
 
-   }
-    console.log(a)}
+//comparision operators
+let comp1=5;
+let comp2="5";
+console.log('comp1==comp2 is', comp1 ==comp2) //eqaul to
+console.log('comp1==comp2 is', comp1 !=comp2)//not equal to
+console.log('comp1==comp2 is', comp1 !==comp2)//not equal value or not equal type
+console.log('comp1==comp2 is', comp1 ===comp2)//equal value and type
 
-    // "use strict"
-    // false//boolean
-    // "true"// string
-    // Document.title//string
-    // "some string".length//number
-    // null//object//
-    
-
-
-//string,number,float,array,boolen,object;
-const am =10;
-var b=am+10;
-console.log(am);
-function myfunction(){
-    let a=10;
-    if(a>5){
-       let a=20;//locally variable declare garna
-       console.log(am);
-
-   }
-    console.log(a)
-
-}
-myfunction()
-
-function myfunction(){
-   var a=10;
-   if(a>5){
-       var a=20;//globally variable declare garna
-       console.log(a);
-
-   }
-   console.log(a)
-
-}
-myfunction()
-let m="this is example of string"
-let b=10
-let c=10.4
-let d=true
-let e=[1,2,"this",true]
-
-undefined,null
-console.log(typeof d)
-let hasDriversLicense=false;
-const passTest=true;
-if(passTest){
-    hasDriversLicense=true;
-
-}
-if(hasDriversLicense){
-    console.log("I am a driver");
-}
-// let private=10
-// console.log(private)
-// array manupulation
-// let array1=[1,2,3,4,5,6];
-// array1.push(20);//to add value at last
-// console.log(array1);
-// array1.pop(7);// to remove last value
-// console.log(array1)
-// array1.unshift(100);
-// console.log(array1)
-// array1.shift()
-// console.log(array1)
-
-let array1=["apple","cat","orange","ball"];
-let ar = array1.indexOf("ball");
-console.log(ar)//if there is not element then it will show -1
-
-//alert(array1.includes("d"));//shows the alert popup
-let firstName="Manisha";
-let lastName ="Tamang";
-let age=19;
-let group ="L4CG7"
-//console.log("My first name is " + firstName + " and my last name is "+ lastName );
-
-//string literals
-console.log(`My name is ${firstName} and my last name is ${lastName} .My age is ${age} and my group is ${group}.`)
+//logical operators
+let a= 2;
+let b= 6;
+console.log(a>b && a==2)
+console.log(a<b && b!==2)
+console.log(!false)
+console.log(!true)
+/*  Tthis is multiline comments*/
+///this is used for single line comment
