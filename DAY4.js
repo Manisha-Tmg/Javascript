22a294a973addfbcbcb57f1f6d9129e99486b948
manishatamang752@gmail.com <manishatamang752@gmail.com>
2023-07-27 23:14:57 +0545

    my first commit

--- DAY4.js
+++ DAY4.js
@@ -1,31 +1,39 @@
 "use strict"
 //loops
 //add first 10 natural numbers
- for( let i=0; i<=20;i++){
-   console.log(i+1)
- }
+//  for( let i=0; i<=20;i++){
+//    console.log(i+1)
+//  }
 
-let n=prompt("enter the svalue of n")
-let sum=0 
-n=Number.parseInt(n)
-for(i = 0;i<n; i++){
-   sum+=(i+1)
+// let n=prompt("enter the svalue of n")
+// let sum=0 
+// n=Number.parseInt(n)
+// for(i = 0;i<n; i++){
+//    sum+=(i+1)
 
- }
- console.log("Sum of first",+ n + "natural numbers is" + sum)
+//  }
+//  console.log("Sum of first",+ n + "natural numbers is" + sum)
 
 
- function sumOfEvenNumbers(arr) {
-   let sum = 0;
-   for (let i = 0; i < arr.length; i++) {
-     if (arr[i] % 2 === 0) {
-       sum += arr[i];
-     }
-   }
-   return sum;
- }
+//  function sumOfEvenNumbers(arr) {
+//    let sum = 0;
+//    for (let i = 0; i < arr.length; i++) {
+//      if (arr[i] % 2 === 0) {
+//        sum += arr[i];
+//      }
+//    }
+//    return sum;
+//  }
  
- 
+ let obj={
+  isha:98,
+  preeyaa:56,
+  neeha:75,
+  aaliyah:60
+ }
+ for (let h in obj){
+  console.log("Marks of " + h + "are" + obj[h])
+ }
 
  
 
