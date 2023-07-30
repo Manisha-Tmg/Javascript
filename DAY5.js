a5e5ac7d4cbdb34e67247555a5089811d5f426d0
manishatamang752@gmail.com <manishatamang752@gmail.com>
2023-07-30 21:57:22 +0545

    my first commit

---
+++ DAY5.js
@@ -0,0 +1,14 @@
+let a=1;
+let b=2;
+function onePlusAvg(a,b){
+    return 1+(x+y)/2
+}
+console.log("one plus Average of a and b is ",1+(a+b)/2)
+
+
+let m=9;
+let n=5;
+function onePlusAvg(m,n){
+    return 1+(m+n)/2
+}
+console.log("one plus Average of a and b is ",1+(a+b)/2)
